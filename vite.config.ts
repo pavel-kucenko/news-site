a142dab22f0f088c18b7322635158a0bc01af9f1
Pavel Kutsenko <smartflaber@gmail.com>
2024-07-13 15:14:59 +0300

    setting up routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { flatRoutes } from 'remix-flat-routes';
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,18 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+      },
+      routes: async (defineRoutes) => {
+        return flatRoutes('routes', defineRoutes, {
+          ignoredRouteFiles: [
+            '.*',
+            '**/*.css',
+            '**/*.test.{js,jsx,ts,tsx}',
+            '**/__*.*',
+            '**/*.server.*',
+            '**/*.client.*',
+          ],
+        });
       },
     }),
     tsconfigPaths(),
